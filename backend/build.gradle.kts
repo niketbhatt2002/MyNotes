45eb1cdf07c82c62ecf3b30f00e5c7f9bfefa509
Niket Bhatt <120054912+niketbhatt2002@users.noreply.github.com>
2026-01-21 23:24:04 -0500

    Update build.gradle.kts to add flyway dependency

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+    implementation("org.flywaydb:flyway-core:9.22.0")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
